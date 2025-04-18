414556a8b917dbcb13ef3d8514622df02fb911ce
fazahumam <fazahumam775@gmail.com>
2025-04-18 23:51:52 +0700

    GitHub

---
+++ app/Models/Barang.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Barang extends Model
+{
+    protected $table = 'barang';
+    protected $fillable = [
+        'kode',
+        'nama',
+        'harga',
+        'stok'
+    ];
+}
---
+++ database/seeders/BarangSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BarangSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('barangs')->insert([
+            [
+                'kode' => 'B001',
+                'nama' => 'Kipas Angin',
+                'harga' => 15000.00,
+                'stok' => 100,
+            ],
+            [
+                'kode' => 'B002',
+                'nama' => 'Televisi',
+                'harga' => 200000.00,
+                'stok' => 50,
+            ],
+        ]);
+    }
+}
