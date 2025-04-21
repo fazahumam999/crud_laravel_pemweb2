8ea79f412546c2cc3591877704f2000975ce58a7
fazahumam999 <fazahumam775@gmail.com>
2025-04-21 14:09:11 +0700

    Add barang.blade.php view to display barangs data

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Barang;
+use Illuminate\View\View;
 use Illuminate\Http\Request;
 
 class BarangController extends Controller
 {
-    //
+    public function barang(): View
+    {
+        $barangs = Barang::all();
+        return view('barang', compact('barangs'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\BarangController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -9,3 +11,5 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/barang', [BarangController::class, 'barang']);
